57bbd93769bf30769c032c4a0885c17d6108d80f
nata bagus <natabagusarisandi68@gmail.com>
2023-08-16 01:25:49 +0700

    set localStorage

--- src/features/auth/login-model.ts
+++ src/features/auth/login-model.ts
@@ -2,9 +2,13 @@ import { ref } from "vue";
 import { User } from "../../domain/model/user";
 import { AuthRepository } from "../../domain/repository/auth-repository";
 import { AuthApiRepository } from "../../data/auth-api-repository";
+import { useRouter } from "vue-router";
 
 export const useLogin = () => {
+  const router = useRouter();
   const authRepo: AuthRepository = new AuthApiRepository();
+  const loading = ref<boolean>(false);
+  const errors = ref<boolean>(false);
   const form = ref<User>(
     User.create({
       email: "",
@@ -24,13 +28,21 @@ export const useLogin = () => {
   };
 
   const handleSubmit = async (e: Event) => {
+    e.preventDefault();
+    loading.value = true;
     try {
-      e.preventDefault();
-      await authRepo.Login(JSON.parse(JSON.stringify(form.value)));
-    } catch (error) {}
+      const auth = await authRepo.Login(JSON.parse(JSON.stringify(form.value)));
+      await localStorage.setItem("token", auth.token);
+      router.push("/");
+    } catch (error) {
+      errors.value = true;
+      loading.value = false;
+    }
   };
   return {
     form,
+    loading,
+    errors,
     handleChange,
     handleSubmit,
   };

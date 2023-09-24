4a6bc0fdf5b6314dcce64a0c81998d22a46564d7
Nata bagus Arisandi <natabagusarisandi68@gmail.com>
2023-09-24 12:42:20 +0700

    clear console

--- src/features/admin/admin-layout/layout-model.ts
+++ src/features/admin/admin-layout/layout-model.ts
@@ -3,7 +3,6 @@ import { onMounted, ref } from "vue";
 export const useLayout = () => {
   const sideBarShow = ref<boolean>(true);
   const handleSideBarShow = () => {
-    console.log("click");
     sideBarShow.value = !sideBarShow.value;
   };
   onMounted(() => {
